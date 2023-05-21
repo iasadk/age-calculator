8793f02530e37d53b33c264756d30c64cf4645d4
iasadk <89456039+iasadk@users.noreply.github.com>
2023-05-21 11:36:02 +0530

    Added Check For Valid Date for this type of Dates: "The date is invalid e.g. 31/04/1991 (there are 30 days in April)"

--- app.js
+++ app.js
@@ -47,12 +47,18 @@ form.addEventListener("submit", (e) => {
 
   //   Calculating The Difference in term of Days, Months and Year
   const formattedDate = formatEnteredDate(day, month, year);
-  const finalResult = calculateDifferenceInDays(formattedDate);
+  if(isValidDate(formattedDate)){
+    const finalResult = calculateDifferenceInDays(formattedDate);
+    // Adding New Results Messages:
+    document.querySelector(".day-placeholder").textContent = `${finalResult[0]}`;
+    document.querySelector(".month-placeholder").textContent = `${finalResult[1]}`;
+    document.querySelector(".year-placeholder").textContent = `${finalResult[2]}`;
+  }else{
+    addThrowErrorClass("month");
+    addThrowErrorClass("day");
+    return;
+  }
 
-  // Adding New Results Messages:
-  document.querySelector(".day-placeholder").textContent = `${finalResult[0]}`;
-  document.querySelector(".month-placeholder").textContent = `${finalResult[1]}`;
-  document.querySelector(".year-placeholder").textContent = `${finalResult[2]}`;
 
 });
 
@@ -119,3 +125,36 @@ const calculateDifferenceInDays = (enteredDate) => {
   const diffYears = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365.25)); // Approximate number of days in a year
   return [diffDays, diffMonths, diffYears]
 };
+
+function isValidDate(date) {
+ 
+  // Date format: YYYY-MM-DD
+  var datePattern = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+
+  // Check if the date string format is a match
+  var matchArray = date.match(datePattern);
+  if (matchArray == null) {
+      return false;
+  }
+
+  // Remove any non digit characters
+  var dateString = date.replace(/\D/g, ''); 
+
+  // Parse integer values from the date string
+  var year = parseInt(dateString.substr(0, 4));
+  var month = parseInt(dateString.substr(4, 2));
+  var day = parseInt(dateString.substr(6, 2));
+ 
+  // Define the number of days per month
+  var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  // Leap years
+  if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+      daysInMonth[1] = 29;
+  }
+
+  if (month < 1 || month > 12 || day < 1 || day > daysInMonth[month - 1]) {
+      return false;
+  }
+  return true;
+}
